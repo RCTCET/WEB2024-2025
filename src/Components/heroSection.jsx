91c30251438cc34c1a836da40d8176cb49884d0c
prancodes <prancoder@gmail.com>
2024-09-24 02:52:09 +0530

    Resolve conflicts

--- src/Components/heroSection.jsx
+++ src/Components/heroSection.jsx
@@ -38,11 +38,7 @@ const RotaractClubLayout = () => {
                         <div key={index} className="flex-1 p-2 ">
                             <div className="relative w-64 h-72 overflow-hidden transform skew-x-12" >
                            < img
-<<<<<<< HEAD
-              src={`/home/img${index + 4}.svg`} alt={`Image ${index + 4}`}
-=======
               src={`/home/hero/img${index + 5}.jpg`} alt={`Image ${index + 5}`}
->>>>>>> 47c7d6f685c193368d12a7b7834b79378eb9a24b
               className={`absolute top-0 left-0 w-full h-full object-cover -skew-x-12 scale-150  ${index%2==0 ? "opacity-100":"opacity-40"}`}
             />
                                 
