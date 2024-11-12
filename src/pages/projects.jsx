f656550ad014bdacd6ed2fb244815abad92844d2
Ahtesham <1032230119@tcetmumbai.in>
2024-11-12 23:28:29 +0530

    Changed Images of Projects Page to WEBp and added lazy loading tag

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -9,22 +9,22 @@ const EndeavorsCarousel = () => {
             avenue: 'Community Service',
             slides: [
                 {
-                    image: '/projects/IMG_0145.JPG',
+                    image: '/projects/IMG_0145.webp',
                     title: 'KALAKRITI',
                     description: 'An initiative where we taught underprivileged children the art of making Ganpati idols using clay. This heartwarming event not only nurtured creativity but also created lasting memories with the kids, fostering joy and learning.',
                 },
                 {
-                    image: '/projects/IMG_9256.jpg',
+                    image: '/projects/IMG_9256.webp',
                     title: 'MUDITA',
                     description: 'A heartfelt visit to an old age home, where we donated gently used clothes, conducted a medical camp, and spent quality time interacting with the elderly.',
                 },
                 {
-                    image: '/projects/COF.jpg',
+                    image: '/projects/COF.webp',
                     title: 'CUP OF COMFORT',
                     description: 'A project aimed at supporting the underprivileged by distributing hot tea and biscuits, fostering a sense of community and care among all involved.',
                 },
                 {
-                    image: '/projects/comserv_cloth_donation__drive.jpg',
+                    image: '/projects/comserv_cloth_donation__drive.webp',
                     title: 'Threads of Kindness',
                     description: 'A compassionate cloth donation drive aimed at collecting gently used clothing for those in need, fostering generosity and care.',
                 },
@@ -34,12 +34,12 @@ const EndeavorsCarousel = () => {
             avenue: 'Professional',
             slides: [
                 {
-                    image: '/projects/IMG_7218.jpg',
+                    image: '/projects/IMG_7218.webp',
                     title: 'Budget 2024: Under the Lens',
                     description: 'An event that enhanced professional skills through engaging debates and discussions on the 2024 Union Budget and key financial policies.',
                 },
                 {
-                    image: '/projects/Awareness and Prevention .jpg',
+                    image: '/projects/Awareness and Prevention .webp',
                     title: 'LinkedIn Launchpad',
                     description: 'A training session where participants learned to optimize their LinkedIn profiles and network strategically for professional growth.',
                 },
@@ -49,7 +49,7 @@ const EndeavorsCarousel = () => {
             avenue: 'Shiksha',
             slides: [
                 {
-                    image: '/projects/IMG20240719115356.jpg',
+                    image: '/projects/IMG20240719115356.webp',
                     title: 'TechWorld',
                     description: 'An engaging initiative designed to teach 9th and 10th-grade students the fundamentals of electronics and digital circuits. Through hands-on activities and interactive sessions, participants gained practical knowledge and skills, sparking their interest in technology and innovation.',
                 },
@@ -64,7 +64,7 @@ const EndeavorsCarousel = () => {
                     description: 'An exciting online gaming tournament where participants competed in a variety of thrilling games.',
                 },
                 {
-                    image: '/projects/IMG_6620.jpg',
+                    image: '/projects/IMG_6620.webp',
                     title: 'Monsoon Matchday',
                     description: 'A thrilling football tournament held during the monsoon, filled with competitive spirit and teamwork.',
                 },
@@ -74,12 +74,12 @@ const EndeavorsCarousel = () => {
             avenue: 'Club Service',
             slides: [
                 {
-                    image: '/projects/IMG_8238.JPG',
+                    image: '/projects/IMG_8238.webp',
                     title: 'CREWMATE CONSPIRACY',
                     description: 'A real-life adaptation of the popular online game Among Us, where participants were split into crewmates and impostors, each with their own secret objectives. Through a series of tasks, challenges, and detective work, the event encouraged teamwork, strategy, and social interaction. It offered a unique and engaging experience, fostering bonding among members while attracting new participants to the club.',
                 },
                 {
-                    image: '/projects/IMG_7522.HEIC (1).jpg',
+                    image: '/projects/IMG_7522.HEIC (1).webp',
                     title: 'SUNDAY FUNDAY',
                     description: 'Sunday Funday 2.0" was a vibrant and engaging event organised by the RCTCET to provide a fun-filled break for its members. Held at Band Stand, the event featured a series of exciting games like Red Light, Green Light, Kho-Kho, and a cricket match, fostering teamwork and camaraderie. The day concluded with a garba dance, instrumental performances.',
                 },
@@ -89,7 +89,7 @@ const EndeavorsCarousel = () => {
             avenue: 'Editorial',
             slides: [
                 {
-                    image: '/projects/IMG20240906131827.jpg',
+                    image: '/projects/IMG20240906131827.webp',
                     title: 'TEACHERS DAY',
                     description: 'To honour and appreciate the hard work and dedication of the teachers and gurus in our college, we created heartfelt thank you notes and personalised greeting cards. This gesture of gratitude was a small way to acknowledge their invaluable contributions to shaping our lives and education.',
                 },
@@ -99,7 +99,7 @@ const EndeavorsCarousel = () => {
             avenue: 'Entrepreneurship Development',
             slides: [
                 {
-                    image: '/projects/IMG_9948.JPG',
+                    image: '/projects/IMG_9948.webp',
                     title: 'Brand Revival Series',
                     description: 'Aimed at enhancing the Entrepreneurial Skills of our members, this event featured a series of fun and competitive games. Participants demonstrated their entrepreneurial knowledge by tackling challenges like understanding profit models and analysing brand ideologies, making it both an educational and engaging experience.',
                 },
@@ -109,7 +109,7 @@ const EndeavorsCarousel = () => {
             avenue: 'Partner in Service',
             slides: [
                 {
-                    image: '/projects/IMG_0107.jpg',
+                    image: '/projects/IMG_0107.webp',
                     title: 'Rotary Medical Camp',
                     description: 'Organized in collaboration with the Rotary Club of Mumbai Borivali East, this free medical camp aimed to provide essential health services to the community. With a team of dedicated volunteers and medical professionals, we offered check-ups, consultations, and health education, making healthcare accessible to those in need.',
                 },
@@ -119,7 +119,7 @@ const EndeavorsCarousel = () => {
             avenue: 'Social Media Relations',
             slides: [
                 {
-                    image: '/projects/IMG_1526.jpg',
+                    image: '/projects/IMG_1526.webp',
                     title: 'Snap and Stroll',
                     description: 'A captivating photowalk set in the picturesque surroundings of South Bombay, this event encouraged participants to embrace their creativity while adhering to specific rules for capturing stunning images. The journey concluded with a breathtaking sunset, providing the perfect backdrop for sharing memories and showcasing the beauty of the location.',
                 },
@@ -129,12 +129,12 @@ const EndeavorsCarousel = () => {
             avenue: 'International Service',
             slides: [
                 {
-                    image: '/projects/International Service_Drowning prevention day.jpg',
+                    image: '/projects/International Service_Drowning prevention day.webp',
                     title: 'DROWNING PREVENTION DAY',
                     description: 'We honoured the lifeguards of Drishti Marines at Juhu Beach. The event aimed to recognize the crucial role of lifeguards and raise awareness about drowning prevention. The team engaged the lifeguards in heartfelt conversations, learning about their challenges and experiences, while also providing them with thoughtful gifts and food boxes as tokens of appreciation.',
                 },
                 {
-                    image: '/projects/IMG_9419.jpg',
+                    image: '/projects/IMG_9419.webp',
                     title: 'World Democracy Day',
                     description: 'An enlightening event where we conducted a demonstration of the current voting system, allowing school students to experience the democratic process firsthand. This initiative aimed to educate young minds about the importance of voting and civic engagement, fostering a sense of responsibility and awareness in the next generation.',
                 },
@@ -216,6 +216,7 @@ const EndeavorsCarousel = () => {
                                 src={slide.image}
                                 alt={slide.title}
                                 className="w-full h-[20rem] md:h-[32rem] object-cover"
+                                loading="lazy"
                             />
                             
                             <div className="absolute bottom-0 left-0 w-full bg-black/60 p-4">
